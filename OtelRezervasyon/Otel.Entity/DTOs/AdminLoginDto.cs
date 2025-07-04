fe10530f56e9d6e80788d3475d87b01cb280af59
Mustafa Enes Gunay <mustafaenesgunay@gmail.com>
2025-07-04 14:26:44 +0300

    E-posta Gönderim Sistemi projesi eklendi

--- OtelRezervasyon/Otel.Entity/DTOs/AdminLoginDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Otel.Entity.DTOs
-{
-    public class AdminLoginDto
-    {
-        public string KullaniciAdi { get; set; } = string.Empty;
-        public string Sifre { get; set; } = string.Empty;
-        public bool BeniHatirla { get; set; }
-    }
-}
